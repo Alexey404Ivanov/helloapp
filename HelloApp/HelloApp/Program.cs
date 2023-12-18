75ef9c831d926c6f6d6a77c787ec9034a0db8716
white <white@example.com>
2023-12-18 20:15:53 +0500

    greet white

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -2,6 +2,11 @@
 
 public class Program
 {
+    public static void GreetWhite()
+    {
+        Console.WriteLine("hello white");
+    }
+
     public static void Main()
     {
         Console.WriteLine("hello");
